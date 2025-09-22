6ed9ab1dd772a654421f574c1b95bf26fb86bb70
Aljosa Vister <aljosa.vister@gmail.com>
2025-09-22 13:25:20 +0200

    Working on sandox

--- sandbox-app/src/config/project.config.extension.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-// Extend this schema with project-specific configuration sections.
-export const projectExtrasSchema = z.object({});
-
-export type ProjectExtras = z.infer<typeof projectExtrasSchema>;
