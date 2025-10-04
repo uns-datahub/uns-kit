07a7a508c4037ce98fb69cc2ba27d1fa246fcaf9
Aljoša Vister <aljosa.vister@gmail.com>
2025-10-04 21:38:01 +0200

    Remove demo

--- packages/uns-demo/src/examples/uns-gateway.ts
+++
@@ -1,7 +0,0 @@
-import { startUnsGateway } from "../../../uns-core/src/uns-grpc/uns-gateway-server.js";
-
-const addr = await startUnsGateway();
-console.log(`UNS Gateway listening on ${addr.address} (UDS=${addr.isUDS})`);
-// Keep alive
-setInterval(() => {}, 1 << 30);
-
--- packages/uns-demo/src/uns/uns-topics.ts
+++
@@ -1,2 +0,0 @@
-export type UnsTopics = "example/" |"example/data-number/" |"example/summary-1/" |"example/summary-2/" |
-(string & {});
