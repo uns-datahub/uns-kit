062bbdb5827c7d0e9127507296b35d7199d03ad7
Aljosa Vister <aljosa.vister@gmail.com>
2026-01-08 13:00:57 +0100

    Fix cron example

--- packages/uns-cli/templates/cron/src/examples/cron-example.ts
+++ packages/uns-cli/templates/cron/src/examples/cron-example.ts
@@ -1,16 +1,20 @@
 /**
  * Change this file according to your specifications and rename it to index.ts
  */
-import { UnsProxyProcess, ConfigFile, logger } from "@uns-kit/core";
-import type { IUnsMessage, UnsEvents } from "@uns-kit/core";
+import { UnsProxyProcess, ConfigFile, logger, UnsEvents } from "@uns-kit/core";
+import { registerAttributeDescriptions, registerObjectTypeDescriptions } from "@uns-kit/core/uns/uns-dictionary-registry.js";
 import "@uns-kit/cron";
-import { type UnsProxyProcessWithCron } from "@uns-kit/cron.js";
-import { UnsTags } from "../uns/uns-tags.js";
-import { UnsTopics } from "../uns/uns-topics.js";
-import { PhysicalMeasurements } from "@uns-kit/core/uns/uns-measurements.js";
+import { type UnsProxyProcessWithCron } from "@uns-kit/cron";
+import { UnsTopics } from "@uns-kit/core/uns/uns-topics.js";
 import { UnsPacket } from "@uns-kit/core/uns/uns-packet.js";
-import { ObjectTypes } from "@uns-kit/core/uns/uns-object.js";
-import { EquipmentAttributes } from "@uns-kit/core/uns/uns-attributes.js";
+import {
+  GeneratedObjectTypes,
+  GeneratedAttributes,
+  GeneratedAttributesByType,
+  GeneratedAttributeDescriptions,
+  GeneratedObjectTypeDescriptions,
+} from "./uns/uns-dictionary.generated.js";
+import { GeneratedPhysicalMeasurements } from "./uns/uns-measurements.generated.js";
 
 
 /**
@@ -19,12 +23,14 @@ import { EquipmentAttributes } from "@uns-kit/core/uns/uns-attributes.js";
  * In the development environment, you are responsible for creating and maintaining this file and its contents.
  */
 const config = await ConfigFile.loadConfig();
+registerObjectTypeDescriptions(GeneratedObjectTypeDescriptions);
+registerAttributeDescriptions(GeneratedAttributeDescriptions);
 
 /**
  * Connect to the output broker and create a crontab proxy
  */
 const unsProxyProcess = new UnsProxyProcess(config.infra.host!, {processName:config.uns.processName}) as UnsProxyProcessWithCron;;
-const mqttOutput = await unsProxyProcess.createUnsMqttProxy((config.output?.host)!, "templateUnsRttOutput", config.uns.instanceMode, config.uns.handover, { publishThrottlingDelay: 1000});
+const mqttOutput = await unsProxyProcess.createUnsMqttProxy((config.output?.host)!, "templateUnsRttOutput", config.uns.instanceMode!, config.uns.handover!, { publishThrottlingDelay: 1000});
 const cronInput = await unsProxyProcess.createCrontabProxy("* * * * * *");
 
 /**
@@ -35,20 +41,24 @@ cronInput.event.on("cronEvent", async (event: UnsEvents["cronEvent"]) => {
   try {
     const time = UnsPacket.formatToISO8601(new Date());
     const numberValue: number = 42;
-    const message: IUnsMessage = { data: { time, value: numberValue, uom: PhysicalMeasurements.MiliVolt } };
     const topic: UnsTopics = "example/";
-    const objectType = ObjectTypes.Equipment;
-    const objectId = "main";
-    const tags: UnsTags[] = [];
-    const packet = await UnsPacket.unsPacketFromUnsMessage(message);
+    const asset = "asset";
+    const assetDescription = "Sample asset";
+
+    const dataGroup = "sensor";
+
     mqttOutput.publishMqttMessage({
       topic,
-      attribute: EquipmentAttributes.MeasuredTemperature,
-      packet,
-      description: "Number value",
-      objectType,
-      objectId,
-      tags
+      asset,
+      assetDescription,
+      objectType: GeneratedObjectTypes.equipment,
+      objectId: "main",
+      attributes: [
+        {
+          attribute: GeneratedAttributesByType["energy-resource"]["current"],
+          data: { dataGroup, time, value: numberValue, uom: GeneratedPhysicalMeasurements.Ampere }
+        }
+      ],
     });
   } catch (error) {
     const reason = error instanceof Error ? error : new Error(String(error));
